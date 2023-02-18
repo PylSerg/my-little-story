b04a14c83da4b9186c01409fb493948e04a0d773
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-02-19 00:21:11 +0200

    adds styles

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <RegistrationScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
