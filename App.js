8e534ed4cf9db605c20d67c4c6e2f1556ce5ed28
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-02-20 19:44:10 +0200

    changes screen

--- App.js
+++ App.js
@@ -1,39 +1,31 @@
 import React from "react";
-import {
-  StyleSheet,
-  TouchableWithoutFeedback,
-  Keyboard,
-  ImageBackground,
-  View,
-} from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, Keyboard, ImageBackground, View } from "react-native";
 
 import RegistrationScreen from "./Screens/RegistrationScreen";
-// import LoginScreen from "./Screens/LoginScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
-  return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.background}
-          source={require("./assets/images/background.jpg")}
-        >
-          <RegistrationScreen />
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={styles.container}>
+				<ImageBackground style={styles.background} source={require("./assets/images/background.jpg")}>
+					<RegistrationScreen />
+					{/* <LoginScreen /> */}
+				</ImageBackground>
+			</View>
+		</TouchableWithoutFeedback>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#000",
+	},
 
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-  },
+	background: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center",
+	},
 });
