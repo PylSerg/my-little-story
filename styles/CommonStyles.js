b8f2d6e0b970ff7a20d745741a1585817de7b648
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-02-20 17:58:44 +0200

    adds common styles

---
+++ styles/CommonStyles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+export default CommonStyles = StyleSheet.create({
+	input: {
+		width: "100%",
+		padding: 16,
+
+		color: "#212121",
+		fontSize: 16,
+		lineHeight: 19,
+
+		backgroundColor: "#f6f6f6",
+
+		borderWidth: 1,
+		borderStyle: "solid",
+		borderColor: "#e8e8e8",
+		borderRadius: 8,
+	},
+
+	inputActive: {
+		backgroundColor: "#fff",
+		borderColor: "#ff6c00",
+	},
+
+	button: {
+		width: "100%",
+
+		padding: 16,
+
+		color: "#fff",
+		fontSize: 16,
+		lineHeight: 19,
+		textAlign: "center",
+
+		backgroundColor: "#ff6c00",
+		borderRadius: 100,
+	},
+});
