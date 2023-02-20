d3ac750be119c2da42f9ae72a17f74bd84d82fc2
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-02-20 18:03:37 +0200

    adds style to RegistrationScreen

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,130 +1,164 @@
-import React from "react";
-import { StyleSheet, View, Text, TextInput, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TouchableWithoutFeedback, Keyboard, View, Text, TextInput, Image } from "react-native";
+import CommonStyles from "../styles/CommonStyles";
 
 export default function RegistrationScreen() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.registrationBlock}>
-        <View style={styles.avatar}>
-          <Image
-            style={styles.addButton}
-            source={require("../assets/images/add.png")}
-          />
-        </View>
+	const [login, setLogin] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [hidePassword, setHidePassword] = useState(true);
 
-        <Text style={styles.header}>Registration</Text>
+	const defaultInputStyle = [CommonStyles.input, styles.input];
+	const activeInputStyle = [...defaultInputStyle, CommonStyles.inputActive];
 
-        <View style={styles.inputBlock}>
-          <TextInput style={styles.input} placeholder="Login" />
+	const [loginStyle, setLoginStyle] = useState(defaultInputStyle);
+	const [emailStyle, setEmailStyle] = useState(defaultInputStyle);
+	const [passwordStyle, setPasswordStyle] = useState(defaultInputStyle);
 
-          <TextInput style={styles.input} placeholder="Email" />
+	// Toggle password visibility
+	const togglePasswordVisibility = () => setHidePassword(!hidePassword);
 
-          <View style={{ width: "100%" }}>
-            <TextInput style={styles.input} placeholder="Password" />
+	// Changes input value
+	const handleChangeLogin = value => setLogin(value);
+	const handleChangeEmail = value => setEmail(value);
+	const handleChangePassword = value => setPassword(value);
 
-            <Text style={styles.showPassword}>Show</Text>
-          </View>
-        </View>
+	// Changes input style to active
+	const changeLoginStyle = () => setLoginStyle(activeInputStyle);
+	const changeEmailStyle = () => setEmailStyle(activeInputStyle);
+	const changePasswordStyle = () => setPasswordStyle(activeInputStyle);
 
-        <Text style={styles.registrationButton}>Registration</Text>
+	// Reset input style
+	const resetLoginStyle = () => setLoginStyle(defaultInputStyle);
+	const resetEmailStyle = () => setEmailStyle(defaultInputStyle);
+	const resetPasswordStyle = () => setPasswordStyle(defaultInputStyle);
 
-        <Text style={styles.signIn}>Already have an account? Sign in</Text>
-      </View>
-    </View>
-  );
+	// Submit form
+	const submitForm = () => {
+		console.log({ login: login, email: email, password: password });
+		setLogin("");
+		setEmail("");
+		setPassword("");
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.registrationBlock}>
+				<View style={styles.avatar}>
+					<Image style={styles.addButton} source={require("../assets/images/add.png")} />
+				</View>
+
+				<Text style={styles.header}>Registration</Text>
+
+				<View style={styles.inputBlock}>
+					<TextInput style={loginStyle} value={login} placeholder="Login" onChangeText={value => handleChangeLogin(value)} onFocus={changeLoginStyle} onBlur={resetLoginStyle} />
+
+					<TextInput
+						style={emailStyle}
+						keyboardType="email-address"
+						value={email}
+						placeholder="Email"
+						onChangeText={value => handleChangeEmail(value)}
+						onFocus={changeEmailStyle}
+						onBlur={resetEmailStyle}
+					/>
+
+					<View style={{ width: "100%" }}>
+						<TextInput
+							style={passwordStyle}
+							secureTextEntry={hidePassword}
+							value={password}
+							placeholder="Password"
+							onChangeText={value => handleChangePassword(value)}
+							onFocus={changePasswordStyle}
+							onBlur={resetPasswordStyle}
+						/>
+
+						<Text style={styles.showPassword} onPress={togglePasswordVisibility}>
+							{hidePassword ? "Show" : "Hidden"}
+						</Text>
+					</View>
+				</View>
+
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<Text style={[CommonStyles.button, styles.registrationButton]} onPress={submitForm}>
+						Registration
+					</Text>
+				</TouchableWithoutFeedback>
+
+				<Text style={styles.signIn}>Already have an account? Sign in</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
+	container: {
+		flex: 1,
+		justifyContent: "flex-end",
+	},
 
-  registrationBlock: {
-    alignItems: "center",
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingTop: 92,
-    backgroundColor: "#fff",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
+	registrationBlock: {
+		alignItems: "center",
+		paddingLeft: 16,
+		paddingRight: 16,
+		paddingTop: 92,
+		backgroundColor: "#fff",
+		borderTopLeftRadius: 25,
+		borderTopRightRadius: 25,
+	},
 
-  avatar: {
-    position: "absolute",
-    top: -60,
-    width: 120,
-    height: 120,
-    backgroundColor: "#f6f6f6",
-    borderRadius: 16,
-  },
+	avatar: {
+		position: "absolute",
+		top: -60,
+		width: 120,
+		height: 120,
+		backgroundColor: "#f6f6f6",
+		borderRadius: 16,
+	},
 
-  addButton: {
-    position: "absolute",
-    right: -12,
-    bottom: 14,
-    width: 25,
-    height: 25,
-  },
+	addButton: {
+		position: "absolute",
+		right: -12,
+		bottom: 14,
+		width: 25,
+		height: 25,
+	},
 
-  header: {
-    fontSize: 30,
-    marginBottom: 32,
-  },
+	header: {
+		fontSize: 30,
+		marginBottom: 32,
+	},
 
-  inputBlock: {
-    width: "100%",
-  },
+	inputBlock: {
+		width: "100%",
+	},
 
-  input: {
-    width: "100%",
-    padding: 16,
+	input: {
+		marginBottom: 16,
+	},
 
-    marginBottom: 16,
+	showPassword: {
+		position: "absolute",
+		right: 16,
+		top: "25%",
 
-    fontSize: 16,
-    lineHeight: 19,
+		color: "#1B4371",
+		fontSize: 16,
+	},
 
-    backgroundColor: "#f6f6f6",
+	registrationButton: {
+		marginTop: 27,
+	},
 
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#e8e8e8",
-    borderRadius: 8,
-  },
+	signIn: {
+		marginTop: 16,
+		marginBottom: 45,
 
-  showPassword: {
-    position: "absolute",
-    right: 16,
-    top: "25%",
-
-    color: "#1B4371",
-    fontSize: 16,
-  },
-
-  registrationButton: {
-    width: "100%",
-
-    marginTop: 27,
-    padding: 16,
-
-    color: "#fff",
-    fontSize: 16,
-    lineHeight: 19,
-    textAlign: "center",
-
-    backgroundColor: "#ff6c00",
-    borderRadius: 100,
-  },
-
-  signIn: {
-    marginTop: 16,
-    marginBottom: 45,
-
-    color: "#1B4371",
-    fontSize: 16,
-    lineHeight: 19,
-  },
+		color: "#1B4371",
+		fontSize: 16,
+		lineHeight: 19,
+	},
 });
 
 // Frame
