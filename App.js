5a102a4300e4cba994b123cdd4d2f28e30719f53
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-02-27 17:18:34 +0200

    adds store

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 import RegistrationScreen from "./screens/auth/RegistrationScreen";
 import LoginScreen from "./screens/auth/LoginScreen";
 import Home from "./screens/main/Home";
@@ -16,43 +19,45 @@ export default function App() {
 	const [isRegistered, setIsRegistered] = useState(false);
 
 	return (
-		<NavigationContainer>
-			<MainStack.Navigator>
-				{!isRegistered && (
-					<>
-						<MainStack.Screen
-							name="Login"
-							component={LoginScreen}
-							options={{
-								headerShown: false,
-							}}
-							initialParams={{ setIsRegistered }}
-						/>
+		<Provider store={store}>
+			<NavigationContainer>
+				<MainStack.Navigator>
+					{!isRegistered && (
+						<>
+							<MainStack.Screen
+								name="Login"
+								component={LoginScreen}
+								options={{
+									headerShown: false,
+								}}
+								initialParams={{ setIsRegistered }}
+							/>
 
-						<MainStack.Screen
-							name="Registration"
-							component={RegistrationScreen}
-							options={{
-								headerShown: false,
-							}}
-							initialParams={{ setIsRegistered }}
-						/>
-					</>
-				)}
+							<MainStack.Screen
+								name="Registration"
+								component={RegistrationScreen}
+								options={{
+									headerShown: false,
+								}}
+								initialParams={{ setIsRegistered }}
+							/>
+						</>
+					)}
 
-				{isRegistered && (
-					<>
-						<MainStack.Screen
-							name="Home"
-							component={Home}
-							options={{
-								headerShown: false,
-							}}
-							initialParams={{ setIsRegistered }}
-						/>
-					</>
-				)}
-			</MainStack.Navigator>
-		</NavigationContainer>
+					{isRegistered && (
+						<>
+							<MainStack.Screen
+								name="Home"
+								component={Home}
+								options={{
+									headerShown: false,
+								}}
+								initialParams={{ setIsRegistered }}
+							/>
+						</>
+					)}
+				</MainStack.Navigator>
+			</NavigationContainer>
+		</Provider>
 	);
 }
